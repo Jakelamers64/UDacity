5bc0cc88bf85c431334fa70eb58996bc0124876b
jake lamers <jakelamers931@gmail.com>
2020-05-27 18:18:56 -0500

    added a function to create data frames that on include sp100 dates

--- CSV_Stuff.py
+++ CSV_Stuff.py
@@ -1,13 +1,33 @@
 import pandas as pd
+import yfinance as yf
 import matplotlib.pyplot as plt
 
 # csv file we are working on
-df = pd.read_csv("C:\\Users\\jakel\\OneDrive\\Desktop\\UDacity\\aapl.csv")
+aapl = pd.read_csv("C:\\Users\\jakel\\OneDrive\\Desktop\\UDacity\\aapl.csv")
+spy = pd.read_csv("C:\\Users\\jakel\\OneDrive\\Desktop\\UDacity\\spy.csv",index_col="Date",parse_dates=True,usecols=['Date','Adj Close'],na_values=['nan'])
+
 
 def create_graph(file):
-    print(file['Adj Close'])
     file[['Close','Adj Close']].plot()
     plt.show()
 
+def create_df():
+    # define date range
+    start_date ='2/6/2020'
+    end_date ='3/6/2020'
+    dates = pd.date_range(start_date,end_date)
+
+    # create empty data frame
+    df1 = pd.DataFrame(index = dates)
+
+    # joins two data frames
+    # spy is sp100 to make sure there was trading that day
+    df1 = df1.join(spy)
+
+    # drops all the nan na_values
+    df1 = df1.dropna()
+
+    return df1
+
 if __name__ == '__main__':
-    create_graph(df)
+    print(create_df())
